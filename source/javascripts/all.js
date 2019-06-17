23cde7f9b0502f1c6a87772a579c610da507d444
shaohua.li <wildimagine@qq.com>
2019-06-17 15:20:55 +0800

    fix: new chapter & wechat img

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
-// This is where it all goes :)
+// This is where it all
+
+var isVisible = false;
+function toggleShowWechat(e) {
+  if(isVisible) {
+    document.getElementById("header-wechat-img").style.visibility="hidden";
+  }else {
+    document.getElementById("header-wechat-img").style.visibility="visible";
+  }
+  isVisible = !isVisible;
+}
+
+window.toggleShowWechat = toggleShowWechat;
